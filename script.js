eb92b361f2b54a96b4d522c7ef4c50eb84a23b55
Ashley Szpanelewski <ashleyszp@gmail.com>
2022-08-17 15:41:03 -0400

    Added save icon to html

--- script.js
+++ script.js
@@ -6,54 +6,55 @@
 5.) Once event is entered, should save to local storage
 6.) Upon refresh, saved event persists
 */
-// save reference to important DOM elements
-var timeDisplayEl = $('#time-display');
+
+// variables
+var timeDisplayEl = $("#time-display");
 
 // handle displaying the time
 function displayTime() {
-    var rightNow = moment().format("MMMM Do, YYYY");
-    timeDisplayEl.text(rightNow);
-  }
+  var rightNow = moment().format("MMMM Do, YYYY");
+  timeDisplayEl.text(rightNow);
+}
 
-  function hourTracker() {
-    //get current hour number (military time)
-    var currentHour = moment().hour();
+function timeBlockTracker() {
+  //get current hour number (military time)
+  var currentHour = moment().hour();
 
-    // loop over time blocks
-    $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log( blockHour, currentHour)
+  // loop over time blocks using the class time block to iterate over each instance
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(blockHour, currentHour);
 
-        //adds css class "past" to style calender hours that are less than current hour
-        if (blockHour < currentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        //adds present class to style the hour block currently happening
-        else if (blockHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        //adds the class future to all hours that have not occurred yet
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
-    })
+    //adds css class "past" to style calender hours that are less than current hour
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    }
+    //adds present class to style the hour block currently happening
+    else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+    }
+    //adds the class future to all hours that have not occurred yet
+    else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
 }
 
 $(".saveBtn").on("click", function () {
-    //get nearby values.
-    console.log(this);
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+  //get nearby values.
+  console.log(this);
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
 
-    //set items in local storage.
-    localStorage.setItem(time, text);
-})
+  //set items in local storage.
+  localStorage.setItem(time, text);
+});
 //load any saved data from LocalStorage - do this for each hour created.
 $("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
@@ -65,5 +66,5 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
-hourTracker();
+timeBlockTracker();
 setInterval(displayTime, 10);
