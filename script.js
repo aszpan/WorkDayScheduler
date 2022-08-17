2219d7085ff5c9eb146e100af632221cabb08a55
Ashley Szpanelewski <ashleyszp@gmail.com>
2022-08-17 14:42:32 -0400

    Added date using moment.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// save reference to important DOM elements
+var timeDisplayEl = $('#time-display');
+
+// handle displaying the time
+function displayTime() {
+    var rightNow = moment().format("MMMM Do, YYYY");
+    timeDisplayEl.text(rightNow);
+  }
+
+
+  
+setInterval(displayTime, 1000);
